08921b38357d55750431cacd7e1905b56d34a976
Zoe Love <zlove@hsph.harvard.edu>
2023-03-05 15:51:58 -0500

    button test

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,13 +11,24 @@ cc = coco.CountryConverter()
 def load_data():
 
     df = pd.read_csv("https://github.com/zoe-love/BMI-706-Project/blob/main/wash_data_cleaned.csv.zip?raw=true", compression = 'zip')
-
     
     # remove country without numeric code (Channel Islands)
     df = df[df['iso3'] != 'CHI']
 
     # add numeric country codes
     df['country-code'] = coco.convert(names = df['iso3'], to='ISOnumeric')
+
+    # filter to only national residence type
+    df = df[df['res_type'] == 'n']
+
+    # filter out any 'arc' variables
+    df = df[(df['level_1'] == 'arc_bas') | (df['level_1'] == 'arc_sm')]
+
+    # pivot table to have column for each unique measure and level_1 coverage amount
+    df = pd.pivot_table(df, values = 'coverage', index = ['name', 'year', 'pop_n', "iso3", "country-code"], columns = ['measure', 'level_1']).reset_index()
+    df.columns = [' '.join(col).strip() for col in df.columns.values]
+
+    
 
     return df
 
@@ -36,9 +47,12 @@ subset = df[df["year"] == year]
 # Type of covereage selection 
 measure = st.radio(
     label = 'Measure Type',
-    options = subset['measure'].unique().tolist()
+    options = ['wat', 'hyg', 'san']
 )
-subset = subset[subset["measure"] == measure]
+country_info = df[['name', 'year', 'pop_n', 'iso3', 'country-code']]
+measure_info = df.loc[:, df.columns.str.startswith(measure)]
+subset = pd.concat([country_info, measure_info.reindex(country_info.index)], axis=1)
+
 
 # Country selection
 countries = st.multiselect(label = 'Country Select', 
@@ -75,7 +89,7 @@ chart = alt.Chart(alt.topo_feature(data.world_110m.url, 'countries')).mark_geosh
 
 st.altair_chart(chart, use_container_width=True)
 
-countries_in_subset = subset["Country"].unique()
+countries_in_subset = subset["name"].unique()
 if len(countries_in_subset) != len(countries):
     if len(countries_in_subset) == 0:
         st.write("No data avaiable for given subset.")
